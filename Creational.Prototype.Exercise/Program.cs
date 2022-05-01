6782b46a0b22d31786fb403a5baa01f1062bd918
aknutman <aknutman@gmail.com>
2022-05-01 08:58:56 +0700

    Creational.Prototype.Exercise

---
+++ Creational.Prototype.Exercise/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Creational.Prototype.Exercise
+{
+    public class Point
+    {
+        public int X, Y;
+    }
+
+    public class Line
+    {
+        public Point Start, End;
+
+        public Line DeepCopy()
+        {
+            // todo
+
+            return new Line()
+            {
+                Start = new Point()
+                {
+                    X = Start.X,
+                    Y = Start.Y
+                },
+                End = new Point()
+                {
+                    Y = End.Y,
+                    X = End.X,
+                }
+            };
+        }
+    }
+
+    public static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
