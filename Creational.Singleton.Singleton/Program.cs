f974b9529e81ce76f18fae36501eee7e31f9baff
aknutman <aknutman@gmail.com>
2022-05-20 21:18:00 +0700

    Creational.Singleton.Singleton

---
+++ Creational.Singleton.Singleton/Program.cs
@@ -0,0 +1,55 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using MoreLinq;
+using Autofac;
+
+namespace DeisgnPatternInCSharp.Creational.Singleton.Singleton
+{
+    public interface IDatabase
+    {
+        int GetPopulation(string name);
+    }
+
+    public class SingletonDatabase : IDatabase
+    {
+        private Dictionary<string, int> capitals;
+
+        private SingletonDatabase()
+        {
+            Console.WriteLine("Initializing database");
+
+            capitals = File.ReadAllLines("capitals.txt")
+                .Batch(2)
+                .ToDictionary(
+                    list => list.ElementAt(0).Trim(),
+                    list => int.Parse(list.ElementAt(1))
+                );
+        }
+
+        public int GetPopulation(string name)
+        {
+            return capitals[name];
+        }
+
+        private static Lazy<SingletonDatabase> instance = new Lazy<SingletonDatabase>(() => new SingletonDatabase());
+
+        public static SingletonDatabase Instance => instance.Value;
+    }
+
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = SingletonDatabase.Instance;
+            var city = "﻿Tokyo";
+            Console.WriteLine($"{city} has population {db.GetPopulation(city)}");
+
+            Console.ReadLine();
+        }
+    }
+}
