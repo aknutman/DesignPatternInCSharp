b70795d0bde08da3f84ea529a78c9c6858e319db
aknutman <aknutman@gmail.com>
2022-05-21 21:08:53 +0700

    Structural.Adapter.WithCaching

--- Structural.Adapter.NoCaching/Program.cs
+++ Structural.Adapter.NoCaching/Program.cs
@@ -1,5 +1,6 @@
 ﻿using MoreLinq;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Dynamic;
@@ -17,6 +18,27 @@ namespace Structural.Adapter.NoCaching
             X = x;
             Y = y;
         }
+
+        protected bool Equals(Point other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Point)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (X * 397) ^ Y;
+            }
+        }
     }
 
     public class Line
@@ -27,6 +49,27 @@ namespace Structural.Adapter.NoCaching
         {
             Start = start ?? throw new ArgumentNullException(paramName: nameof(start));
             End = end ?? throw new ArgumentNullException(paramName: nameof(end));
+        }
+
+        protected bool Equals(Line other)
+        {
+            return Equals(Start, other.Start) && Equals(End, other.End);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Line)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Start != null ? Start.GetHashCode() : 0) * 397) ^ (End != null ? End.GetHashCode() : 0);
+            }
         }
     }
 
@@ -46,13 +89,20 @@ namespace Structural.Adapter.NoCaching
         }
     }
 
-    public class LineToPointAdapter : Collection<Point>
+    public class LineToPointAdapter : IEnumerable<Point>
     {
         private static int count;
 
+        private static Dictionary<int, List<Point>> cache = new Dictionary<int, List<Point>>();
+
         public LineToPointAdapter(Line line)
         {
+            var hash = line.GetHashCode();
+            if (cache.ContainsKey(hash)) return;
+
             Console.WriteLine($"{++count}: Generating points for line [{line.Start.X}, {line.Start.Y}]-[{line.End.X}, {line.End.Y}]");
+
+            var points = new List<Point>();
 
             int left = Math.Min(line.Start.X, line.End.X);
             int right = Math.Max(line.Start.X, line.End.X);
@@ -65,16 +115,28 @@ namespace Structural.Adapter.NoCaching
             {
                 for (int y = top; y <= bottom; ++y)
                 {
-                    Add(new Point(left, y));
+                    points.Add(new Point(left, y));
                 }
             }
             else if (dy == 0)
             {
                 for (int x = left; x <= right; ++x)
                 {
-                    Add(new Point(x, top));
+                    points.Add(new Point(x, top));
                 }
             }
+
+            cache.Add(hash, points);
+        }
+
+        public IEnumerator<Point> GetEnumerator()
+        {
+            return cache.Values.SelectMany(x => x).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 
---
+++ Structural.Adapter.WithCaching/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Structural.Adapter.WithCaching continued in no cache");
