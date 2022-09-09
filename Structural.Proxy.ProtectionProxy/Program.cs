dc761a6a4fef739e3bd4651002e6fb5bb8616ebc
Eldest Pasirula <aknutman@gmail.com>
2022-09-09 21:14:11 +0700

    Structural.Proxy.ProtectionProxy

---
+++ Structural.Proxy.ProtectionProxy/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Structural.Proxy.ProtectionProxy
+{
+    public interface ICar
+    {
+        void Drive();
+    }
+
+    public class Car : ICar
+    {
+        public void Drive()
+        {
+            WriteLine("Car is being driven");
+        }
+    }
+
+    public class Driver
+    {
+        public int Age { get; set; }
+
+        public Driver(int age)
+        {
+            this.Age = age;
+        }
+    }
+
+    public class CarProxy : ICar
+    {
+        public Driver driver;
+        private Car car = new Car();
+
+        public CarProxy(Driver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Drive()
+        {
+            if (driver.Age >= 16)
+            {
+                car.Drive();
+            }
+            else
+            {
+                WriteLine("Too young");
+            }
+        }
+    }
+
+    public class Programm
+    {
+        static void Main(string[] args)
+        {
+            ICar car = new CarProxy(new Driver(12));
+            car.Drive();
+        }
+    }
+}
