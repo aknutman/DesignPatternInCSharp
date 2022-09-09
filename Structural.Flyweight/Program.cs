a1f9b7626b337fb367d3c76ca07721521f0c68c9
Eldest Pasirula <aknutman@gmail.com>
2022-09-09 16:18:27 +0700

    flyweight first

---
+++ Structural.Flyweight/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace Flyweight
+{
+    public class FormattedText
+    {
+        private readonly string plainText;
+        private bool[] capitalize;
+        public FormattedText(string plainText)
+        {
+            this.plainText = plainText;
+            capitalize = new bool[plainText.Length];
+        }
+
+        public void Capitalize(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+                capitalize[i] = true;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (var i = 0; i < plainText.Length; i++)
+            {
+                var c = plainText[i];
+                sb.Append(capitalize[i] ? char.ToUpper(c) : c);
+            }
+            return sb.ToString();
+        }
+    }
+    public class Programm
+    {
+        static void Main(string[] args)
+        {
+            var ft = new FormattedText("This is a brave new world");
+            ft.Capitalize(10, 15);
+            Console.WriteLine(ft);
+        }
+    }
+}
